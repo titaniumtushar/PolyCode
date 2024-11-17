026e90223013e37286e37f23eb929c42f02971d6
Parth <parsjoshi2@gmail.com>
2024-11-18 00:40:40 +0530

    Basic contest creation done

--- server/controllers/createContest.ts
+++ server/controllers/createContest.ts
@@ -1,4 +1,56 @@
-function createContest(req:any,res:any){
-    console.log(req.body,"this is body we sent for this!");
-    const {}
+import mongoose from "mongoose";
+import { contestModel } from "../models/contest";
+
+interface contest{
+  contest_name: string;
+  invitation_code: string;
+  question_set: [object];
+  prize_distribution: object;
+  community_id: string;
+  start_time?: number;
+  end_time?: number;
 }
+
+
+async function createContest(req:any,res:any){
+    const { contest_name, invitation_code, question_set, prize_distribution } =
+        req.body;
+
+
+    if(!contest_name || !invitation_code || !question_set ||  !prize_distribution){
+      return res.status(400).json({message:"All param are required"});
+
+    }
+
+    const contest:contest= {
+        contest_name:contest_name,
+        invitation_code:"46546560",
+        question_set:question_set,
+        prize_distribution:prize_distribution,
+        community_id:req.decoded.id,
+    }
+
+
+    try {
+
+        const newContest = new contestModel({meta:contest});
+
+        await newContest.save();
+
+      return res.status(200).json({message:"Contest Created!"});
+
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({message:"Something went wrong"});
+
+
+    }
+
+
+    
+
+}
+
+
+export {createContest}
--- server/routes/community.ts
+++ server/routes/community.ts
@@ -2,38 +2,24 @@ import express from "express";
 import bcrypt from "bcrypt";
 
 import jwt from "jsonwebtoken";
-import { CommunityModel,  UserModel } from "../models/user";
+import { CommunityModel, UserModel } from "../models/user";
 import { signup } from "./signup";
 import { login } from "./login";
 import { authorizeCommunity, authorizeUser } from "../middlewares/authorize";
+import { createContest } from "../controllers/createContest";
 
 const community = express.Router();
 
 community.use(authorizeCommunity);
 
 community.post("/signup", async (req, res) => {
-  signup(req,res,"C");
+    signup(req, res, "C");
 });
 
 community.post("/login", async (req, res) => {
-  login(req,res,"C");
+    login(req, res, "C");
 });
 
-community.post("/create/contest",(req,res)=>{
-  console.log(req.body);
-  return res.json("hello guys");
-})
+community.post("/create/contest", createContest);
 
-
-
-
-
-
-
-export {community};
-
-
-
-
-
-
+export { community };
