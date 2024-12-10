92920d3bca74307cd3392169ad8d2bddaf897b63
nitish <Nitish Singh>
2024-12-10 23:37:34 +0530

    marketplace done

--- server/routes/user.ts
+++ server/routes/user.ts
@@ -9,23 +9,27 @@ import { registerContest } from "../controllers/registerContest";
 import { CONTEST_SECRET, JWT_SECRET } from "../server";
 import { submitProblems } from "../controllers/submitProblems";
 import { pollContest } from "../utils/mongoPolling";
+import { getProducts } from "./product";
+
 require("dotenv");
 
 const user = express.Router();
 
 // Signup Route
 user.post("/signup", async (req, res) => {
-  signup(req, res, "U");
+    signup(req, res, "U");
 });
+
+user.get("/product/list", getProducts);
 
 // Login Route
 user.post("/login", async (req, res) => {
-  login(req, res, "U");
+    login(req, res, "U");
 });
 
 // Authentication Check
 user.get("/auth", (req, res) => {
-  return res.status(200).json({ message: "Authenticated." });
+    return res.status(200).json({ message: "Authenticated." });
 });
 
 // Contest Routes
@@ -34,52 +38,57 @@ user.post("/contest/register", registerContest);
 
 // Join Contest Route with Token Verification
 user.get("/join/:token", (req, res) => {
-  const { token } = req.params;
+    const { token } = req.params;
 
-  try {
-    const verify = jwt.verify(token, String(CONTEST_SECRET));
-    console.log(verify, "this is coolll");
+    try {
+        const verify = jwt.verify(token, String(CONTEST_SECRET));
+        console.log(verify, "this is coolll");
 
-    if (!verify) {
-      return res.status(500).json({ message: "You cannot join the room." });
-    } else if (!verify.contest_id) {
-      throw new Error("Token does not contain contest ID.");
+        if (!verify) {
+            return res
+                .status(500)
+                .json({ message: "You cannot join the room." });
+        } else if (!verify.contest_id) {
+            throw new Error("Token does not contain contest ID.");
+        }
+
+        res.setHeader("Content-Type", "text/event-stream");
+        res.setHeader("Cache-Control", "no-cache");
+        res.setHeader("Connection", "keep-alive");
+
+        const data = JSON.stringify({
+            message: "Update from Stream 1",
+            timestamp: new Date().toISOString(),
+            contest: verify,
+        });
+        res.write(`data: ${data}\n\n`);
+
+        const sendEvent = async () => {
+            const contestRankings = await pollContest(verify.contest_id);
+            const kapa = {
+                message: "Update from Stream 1",
+                rankings: contestRankings.rankings,
+                timestamp: new Date().toISOString(),
+            };
+            const data = JSON.stringify(kapa);
+            res.write(`data: ${data}\n\n`);
+        };
+
+        sendEvent();
+
+        const interval = setInterval(sendEvent, 4000);
+
+        req.on("close", () => {
+            clearInterval(interval);
+            res.end();
+        });
+    } catch (error) {
+        console.error("Error verifying token:", error);
+        return res.status(500).json({ message: "Invalid token." });
     }
-
-    res.setHeader("Content-Type", "text/event-stream");
-    res.setHeader("Cache-Control", "no-cache");
-    res.setHeader("Connection", "keep-alive");
-
-    const data = JSON.stringify({ message: 'Update from Stream 1', timestamp: new Date().toISOString() ,contest:verify});
-  res.write(`data: ${data}\n\n`);
-
-    const sendEvent = async () => {
-      const contestRankings = await pollContest(verify.contest_id);
-      const kapa = {
-        message: "Update from Stream 1",
-        rankings: contestRankings.rankings,
-        timestamp: new Date().toISOString(),
-      };
-      const data = JSON.stringify(kapa);
-      res.write(`data: ${data}\n\n`);
-    };
-
-    sendEvent();
-
-    const interval = setInterval(sendEvent, 4000);
-
-    req.on("close", () => {
-      clearInterval(interval);
-      res.end();
-    });
-  } catch (error) {
-    console.error("Error verifying token:", error);
-    return res.status(500).json({ message: "Invalid token." });
-  }
 });
 
 // Problem Submission Route
 user.post("/submit", submitProblems);
-
 
 export { user };
